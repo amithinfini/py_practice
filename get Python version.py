78dd21ef9d8e60ce8c466d0950bba29f9ae39058
amithinfini <amith.infini@gmail.com>
2020-08-01 22:06:53 +0530

    Get python version

---
+++ get Python version.py
@@ -0,0 +1,7 @@
+import sys
+print('python version:', sys.version)
+print('\npython version info:', sys.version_info)
+print('\npython version info[major]:', sys.version_info[0])
+print('\npython version info[minor]:', sys.version_info[1])
+print('\npython version info[micro]:', sys.version_info[2])
+print('\npython version info[release level]:', sys.version_info[3])
